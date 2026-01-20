b3657b287a0135f2cca7f163903ca048b69b656a
best-shen <shenys_ujs@163.com>
2026-01-20 21:47:25 +0800

    Complete all exercises

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -51,10 +51,15 @@ int main(int argc, char **argv) {
 
     std::vector<const char *> answers[]{
         {"fd"},
-        // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
-        // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
-        {"", "", "", "", "", "", "", ""},
-        {"", "", "", "", "", "", "", ""},
+        // problems[1]: 在此平台下，参数析构延迟到表达式结束。
+        // Res1("ffr") 和 Res2("d") 都在语句结束时析构。
+        // 按照栈的后进先出顺序：先析构 Res2("d")，后析构 Res1("ffr")。
+        {"d", "ffr"},
+        
+        // problems[2]: 
+        // 涉及 Res1("r"), Res2("d"), Res3("d")。
+        // 按照创建顺序的逆序析构：Res3 -> Res2 -> Res1。
+        {"d", "d", "r"},
     };
 
     // ---- 不要修改以下代码 ----
